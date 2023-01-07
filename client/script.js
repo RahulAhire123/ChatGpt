a03a4071a62e8319cf0b6a0f69151c174afc7259
rahul <rahulnamdeoahire@gmail.com>
2023-01-07 17:27:25 +0530

    uploading project new

--- client/script.js
+++ client/script.js
@@ -86,7 +86,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch('https://chatgpt-ao3b.onrender.com/', {
+    const response = await fetch('https://chatgpt-project-aiii.onrender.com/', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
